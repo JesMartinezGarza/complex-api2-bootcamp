e5209cd290831daf9f98484129bbc5ec4a6cea71
Jesus Martinez-Garza <jes.martinez.garza@gmail.com>
2022-12-03 02:22:33 -0500

    This should be a fully functioning complex API that takes in a query, retrieves information from a first API and then makes a second query to a second API that responds with an image for the first query, presenting the most important pieces of information (i.e. Title, Artist Title, Description, Classification Title(s), Dimensions, Image Id, and Image) in the browser.

--- js/main.js
+++ js/main.js
@@ -11,14 +11,30 @@ function getPainting(){
       console.log(data.data[0])
       let id = data.data[0].id
       let api_link = data.data[0].api_link
-      let description = data.data[0].thumbnail.alt_text
-      let height = data.data[0].thumbnail.height
-      let width = data.data[0].thumbnail.width
+      
       document.querySelector('#api_link').href = api_link
       document.querySelector('#api_link').innerText = api_link
 
-      document.querySelector('#description').innerText = 'Description: ' + description
-      document.querySelector('#dimensions').innerText = 'Dimensions: ' + height + ' x ' + width
+      if(data.data[0].thumbnail === null){
+        console.log('Object has no thumbnail property.')
+        document.querySelector('#description').innerText = 'Description: No description provided.'
+        document.querySelector('#dimensions').innerText = 'Dimensions: No dimensions provided.'
+      }else if(data.data[0].thumbnail.alt_text !== null ){
+
+        let description = data.data[0].thumbnail.alt_text
+        let height = ''
+        let width = ''
+        document.querySelector('#description').innerText = 'Description: ' + description
+
+        if(data.data[0].thumbnail.height === null && data.data[0].thumbnail.width === null){
+          document.querySelector('#dimensions').innerText = 'Dimensions: No dimensions provided.'
+        }else
+          height = data.data[0].thumbnail.height
+          width = data.data[0].thumbnail.width
+          document.querySelector('#dimensions').innerText = 'Dimensions: ' + height + ' x ' + width
+      }else
+        document.querySelector('#description').innerText = 'Description: No description provided.'
+
 
 
 
@@ -28,31 +44,50 @@ function getPainting(){
       .then(data => {
         console.log(data.data)
         console.log(data.data.image_id)
-        console.log('Jesus loves you')
 
-
-        let title = data.data.title
         let image_id = data.data.image_id
+        let title = data.data.title
         let artistTitle = data.data.artist_title
-        document.querySelector('#title').innerText = 'Title: ' + title
 
-        console.log(artistTitle)
+
+        if(image_id === null){
+          image_id = 'No Image Id provided.'
+          document.querySelector('#image_id').innerText = 'Image Id: ' + image_id
+          document.querySelector('#painting').setAttribute('class', 'hidden')
+          console.log(document.querySelector('#painting').getAttribute('src')) 
+          console.log('This is here because image_id does equal null in name and type.')
+        }else if(image_id !== null){
+          document.querySelector('#image_id').innerText = 'Image Id: ' + image_id
+          document.querySelector('#painting').src = `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`
+          console.log('This is here because image_id does not equal null in name and type.')
+          document.querySelector('#painting').removeAttribute('class', 'hidden')
+        }
+
+
+        if(title === null){
+          title = 'No image title provided.'
+        }else if(title !== null){
+          document.querySelector('#title').innerText = 'Title: ' + title
+        }
+
+        if(artistTitle === null){
+          artistTitle = 'No artist title provided.'
+        }else if(artistTitle !== null){
+          document.querySelector('#artistTitle').innerText = 'Artist Title: ' + artistTitle
+        }
 
         let classificationTitles = data.data.classification_titles
-        let classificationsSpacedCorrectly = ''
-        for(let i = 0; i < classificationTitles.length; i++){
-          if(i !== classificationTitles.length - 1){
-            classificationsSpacedCorrectly += classificationTitles[i] + ', '
-
-          }else
-            classificationsSpacedCorrectly += classificationTitles[i]
+        if(classificationTitles !== null && classificationTitles.length !== 0){
+          let classificationsSpacedCorrectly = ''
+          for(let i = 0; i < classificationTitles.length; i++){
+            if(i !== classificationTitles.length - 1){
+              classificationsSpacedCorrectly += classificationTitles[i] + ', '
+  
+            }else
+              classificationsSpacedCorrectly += classificationTitles[i]
+          }
+          document.querySelector('#classificationTitles').innerText = 'Classification Title: ' + classificationsSpacedCorrectly
         }
-        document.querySelector('#artistTitle').innerText = 'Artist Title: ' + artistTitle
-        document.querySelector('#classificationTitles').innerText = 'Classification Title: ' + classificationsSpacedCorrectly
-        document.querySelector('#image_id').innerText = 'Image Id: ' + image_id
-        document.querySelector('#painting').src = `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`
-
-
 
       })
   
